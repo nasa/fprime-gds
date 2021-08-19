c8c1e3bef6f9588a0e24a86849f97b87b0610544
janamian <saba.janamian@jpl.nasa.gov>
2021-08-18 17:48:15 -0700

    saba-ja: updated format_string func

--- src/fprime_gds/common/utils/string_util.py
+++ src/fprime_gds/common/utils/string_util.py
@@ -1,8 +1,6 @@
 """
 string_util.py
-
 Utility functions to process strings to be used in FPrime GDS
-
 @Created March 18, 2021
 @janamian
 """
@@ -10,37 +8,68 @@ Utility functions to process strings to be used in FPrime GDS
 import re
 
 
-def format_string(format_str, values):
+def format_string(format_str, given_values):
     """
     Function to convert C-string style to python format
     without using python interpolation
     Considered the following format for C-string:
     %[flags][width][.precision][length]type
     
-    %:           (?<!%)(?:%%)*%
-    flags:       ([\-\+0\ \#])?
-    width:       (\d+|\*)?
-    .precision:  (\.\*|\.\d+)?
-    length:      `([hLIw]|l{1,2}|I32|I64)?`
-    type:        `([cCdiouxXeEfgGaAnpsSZ])`
+    0- %:                (?<!%)(?:%%)*%
+    1- flags:            ([\-\+0\ \#])?
+    2- width:            (\d+|\*)?
+    3- .precision:       (\.\*|\.\d+)?
+    4- length:          `([hLIw]|l{1,2}|I32|I64)?`
+    5- conversion_type: `([cCdiouxXeEfgGaAnpsSZ])`
     
     Note:
-    This function will keep the flags, width, .precision and length of C-string
+    This function will keep the flags, width, and .precision of C-string
     template. It will remove all types so they could be duck-typed by python 
-    interpreter except for hex type X or x.
+    interpreter except for hex type X or x. lengths will also be removed since
+    they are not meaningful to Python interpreter
+    See: https://docs.python.org/3/library/stdtypes.html#printf-style-string-formatting
+    Regex Source: https://www.regexlib.com/REDetails.aspx?regexp_id=3363
     """
     def convert(match_obj):
         if match_obj.group() is not None:
-            template = "{:" + match_obj.group()[1:-1]
-            if match_obj.group()[-1].lower() == 'x':
-                template += match_obj.group()[-1]
-            template += "}"
+            flags, width, percision, lenght, conversion_type = match_obj.groups()
+            fmr_str = ''
+            if flags:
+                fmr_str += f'{flags}'
+            if width:
+                fmr_str += f'{width}'
+            if percision:
+                fmr_str += f'{percision}'
+
+            if conversion_type:
+                if any([
+                    str(conversion_type).lower() == 'f',
+                    str(conversion_type).lower() == 'd',
+                    str(conversion_type).lower() == 'x',
+                    str(conversion_type).lower() == 'o',
+                    str(conversion_type).lower() == 'e',
+                    ]):
+                    fmr_str += f'{conversion_type}'
+
+            if fmr_str == '':
+                template = '{}'
+            else:
+                template = '{:' + fmr_str + '}'
             return template
         return match_obj
 
-    pattern = "(?<!%)(?:%%)*%([\-\+0\ \#])?(\d+|\*)?(\.\*|\.\d+)?([hLIw]|l{1,2}|I32|I64)?([cCdiouxXeEfgGaAnpsSZ])"
+    # Allowing single, list and tuple inputs
+    if not isinstance(given_values, (list, tuple)):
+        values = (given_values, )
+    elif isinstance(given_values, list):
+        values = tuple(given_values)
+    else:
+        values = given_values
+
+    pattern = '(?<!%)(?:%%)*%([\-\+0\ \#])?(\d+|\*)?(\.\*|\.\d+)?([hLIw]|l{1,2}|I32|I64)?([cCdiouxXeEfgGaAnpsSZ])'
+
     match = re.compile(pattern) 
     formated_str = re.sub(match, convert, format_str)
     result = formated_str.format(*values)
-    result = result.replace("%%", "%")
+    result = result.replace('%%', '%')
     return result
