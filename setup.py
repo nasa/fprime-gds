9114d005d5a6cd649c3393d0c6c0e3efd95a5a34
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2022-04-07 11:06:09 -0700

    lestarch: removing fprime tools version to deconflict

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ integrated configuration with ground in-the-loop.
         "pexpect==4.8.0",
         "pytest==6.2.4",
         "flask_restful==0.3.8",
-        "fprime-tools>=3.0.0",
+        "fprime-tools",
         "argcomplete==1.12.3",
         "Jinja2==2.11.3",
         "Werkzeug==2.0.1",
