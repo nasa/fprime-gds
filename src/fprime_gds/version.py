c7a14f0ae2d29109a5606f267b95f901a2984211
Michael D Starch <Michael.D.Starch@jpl.nasa.gov>
2022-06-29 17:17:36 -0700

    lestarch: disabling dicationay version until it can be replaced with a schema version

--- src/fprime_gds/version.py
+++ src/fprime_gds/version.py
@@ -3,7 +3,7 @@
 This file contains versioning and compatibility definitions for the GDS tool set.
 """
 
-# Currently the GDS is backwards compatible with all dictionaries. This MUST be bumped with each framework release to
-# ensure compatibility
+# Currently the GDS is backwards compatible with all dictionaries. 9,9,9 effectively disables this check. It will be
+# revisited with a schema version in the next release.
 MINIMUM_SUPPORTED_FRAMEWORK_VERSION = (0, 0, 0)
-MAXIMUM_SUPPORTED_FRAMEWORK_VERSION = (3, 0, 0)
+MAXIMUM_SUPPORTED_FRAMEWORK_VERSION = (9, 9, 9)
