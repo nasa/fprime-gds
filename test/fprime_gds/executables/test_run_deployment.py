8d6352eb6b8b2604a43480d8aa30d65c0a3eac92
rmelick <russell@muonspace.com>
2022-07-21 08:32:47 -0700

    Create temporary files so that the deployment tests need less mocking

--- test/fprime_gds/executables/test_run_deployment.py
+++ test/fprime_gds/executables/test_run_deployment.py
@@ -1,4 +1,8 @@
+import platform
+import tempfile
 import unittest
+from pathlib import Path
+
 from unittest import mock
 
 from fprime_gds.executables import run_deployment
@@ -10,6 +14,22 @@ class TestRunDeployment(unittest.TestCase):
         # Same as the "Testing F´ GDS Installation Via Running HTML GUI" from
         # https://nasa.github.io/fprime/INSTALL.html
         # fprime-gds -g html -r <path to fprime checkout>/Ref/build-artifacts
-        with mock.patch("sys.argv", ["main", "-g", "html", "-r", "./build-artifacts"]):
-            with mock.patch("os.path.isdir", return_value=True):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            self.create_fake_deployment_structure(tmpdirname)
+            with mock.patch("sys.argv", ["main", "-g", "html", "-r", tmpdirname]):
                 run_deployment.get_settings()
+
+    def create_fake_deployment_structure(self, temporary_directory):
+        system_dir = Path(temporary_directory) / platform.system()
+
+        bin_dir = system_dir / "bin"
+        bin_dir.mkdir(parents=True)
+        bin_file = bin_dir / "Test"
+        with bin_file.open(mode="wb") as fake_app:
+            fake_app.write("fake app".encode("utf-8"))
+
+        dictionary_dir = system_dir / "dict"
+        dictionary_dir.mkdir(parents=True)
+        dictionary_file = dictionary_dir / "TestTopologyAppDictionary.xml"
+        with dictionary_file.open(mode="w") as fake_dictionary:
+            fake_dictionary.write("<test></test>")
