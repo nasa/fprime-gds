5351038e0945ab9c7df719f65db4ed521e8811bb
ThibFrgsGmz <farges.thibault@gmail.com>
2023-04-22 19:24:55 +1200

    (fix) remove unused variable

--- src/fprime_gds/common/utils/string_util.py
+++ src/fprime_gds/common/utils/string_util.py
@@ -92,7 +92,7 @@ def format_string_template(format_str, given_values):
         result = formatted_str.format(*values)
         result = result.replace("%%", "%")
         return result
-    except Exception as exc:
+    except Exception:
         msg = "Value and format string do not match. "
         msg += " Will ignore integer flags `d` in string template. "
         msg += f"values: {values}. "
