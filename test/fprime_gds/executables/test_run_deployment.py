57291bbac9a4f288a3fb1ac6a98cebd68b790963
Thomas Boyer Chammard <49786685+thomas-bc@users.noreply.github.com>
2023-05-01 16:15:59 -0700

    Update explanation comment

--- test/fprime_gds/executables/test_run_deployment.py
+++ test/fprime_gds/executables/test_run_deployment.py
@@ -13,7 +13,7 @@ class TestRunDeployment(unittest.TestCase):
     def test_as_in_installation_instructions(self):
         # Same as the "Testing F´ GDS Installation Via Running HTML GUI" from
         # https://nasa.github.io/fprime/INSTALL.html
-        # fprime-gds -g html -r <path to fprime checkout>/Ref/build-artifacts
+        # fprime-gds -g html -d <path to fprime checkout>/Ref/build-artifacts/<platform>/Test
         with tempfile.TemporaryDirectory() as temporary_directory:
             self.create_fake_deployment_structure(temporary_directory)
             with mock.patch("sys.argv", ["main", "-g", "html", "-d", str(Path(temporary_directory) / platform.system() / "Test")]):
