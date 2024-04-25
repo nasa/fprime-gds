ad13216da34409973b6912d4a902c0b32f0574da
thomas-bc <thomas.boyerchammard@gmail.com>
2024-04-25 10:29:27 -0700

    Make tests run with Pytest and in CI

--- test/fprime_gds/executables/test_data_product_writer.py
+++ test/fprime_gds/executables/test_data_product_writer.py
@@ -4,25 +4,24 @@ import subprocess
 import os
 import sys
 
-
-# Specify the directory where the .bin files are located
-directory = "./"
-
-# Loop through each file in the specified directory
-for filename in os.listdir(directory):
-    # Check if the file is a .bin file
-    if filename.endswith(".bin"):
-        # Construct the full path to the file
-        file_path = os.path.join(directory, filename)
-        
-        # Construct the command to run
-        command = f"../../../src/fprime_gds/executables/data_product_writer.py {file_path}"
-        
-        # Execute the command
-        process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        
-        # Output the result
-        print(f"Running command: {command}")
-        print(process.stdout.decode())
-        print(process.stderr.decode())
+def test_data_product_writer():
+    # Specify the directory where the .bin files are located
+    directory = os.path.abspath(os.path.dirname(__file__))
+    # Loop through each file in the specified directory
+    for filename in os.listdir(directory):
+        # Check if the file is a .bin file
+        if filename.endswith(".bin"):
+            # Construct the full path to the file
+            file_path = os.path.join(directory, filename)
+            
+            # Construct the command to run
+            command = f"../../../src/fprime_gds/executables/data_product_writer.py {file_path}"
+            
+            # Execute the command
+            process = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            
+            # Output the result
+            print(f"Running command: {command}")
+            print(process.stdout.decode())
+            print(process.stderr.decode())
 
