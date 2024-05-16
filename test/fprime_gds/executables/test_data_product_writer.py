c365954bf25026be273b9e7ad7b7cd097a371eb5
watney <garth.j.watney@jpl.nasa.gov>
2024-05-15 17:14:33 -0700

    In pytest use test directory to access files but use current directory to dump json files

--- test/fprime_gds/executables/test_data_product_writer.py
+++ test/fprime_gds/executables/test_data_product_writer.py
@@ -27,18 +27,20 @@ class TestRunDataProduct(unittest.TestCase):
                     "makeU8Array.bin"]
         
         # Specify the directory where the .bin files are located
-        directory = os.getcwd()
+        cwd_directory = os.getcwd()
+        test_directory = os.path.dirname(os.path.abspath(__file__))
+
         # Loop through each file in the specified directory
         for filename in binFiles:
             print(f'Processing {filename}')
-            bin_file = os.path.join(directory, "dp_writer_data", filename)
-            dict_file = os.path.join(directory, "dp_writer_data", "dictionary.json")
+            bin_file = os.path.join(test_directory, "dp_writer_data", filename)
+            dict_file = os.path.join(test_directory, "dp_writer_data", "dictionary.json")
             args = data_product_writer.parse_args([bin_file, dict_file])
             data_product_writer.process(args)
 
             # Check if the json file was created
             jsonFile = filename.replace('.bin', '.json')
-            jsonFilePath = os.path.join(directory, jsonFile)
+            jsonFilePath = os.path.join(cwd_directory, jsonFile)
             self.assertTrue(os.path.exists(jsonFilePath))
     
             # If the json file exists, delete it
