f13303fc9e2a696a6ad8f2fa480a0bf762e001dd
Zimri Leisher <zimri.leisher@fireflyspace.com>
2024-10-29 17:16:35 -0500

    much more work on parsing cmd args and type ctor args

--- src/fprime_gds/common/sequence/compiler.py
+++ src/fprime_gds/common/sequence/compiler.py
@@ -7,10 +7,13 @@ from argparse import ArgumentParser
 import sys
 import logging
 
+from fprime_gds.common.templates.ch_template import ChTemplate
+from fprime_gds.executables.data_product_writer import IntegerType
+
 logging.basicConfig()
 logger = logging.getLogger(__file__)
-from fprime_gds.common.data_types.cmd_data import CmdData
 from fprime_gds.common.loaders.cmd_json_loader import CmdJsonLoader
+from fprime_gds.common.loaders.ch_json_loader import ChJsonLoader
 from fprime_gds.common.templates.cmd_template import CmdTemplate
 from fprime.common.models.serialize.serializable_type import SerializableType
 from fprime.common.models.serialize.array_type import ArrayType
@@ -28,31 +31,74 @@ from fprime.common.models.serialize.numerical_types import (
 )
 from fprime.common.models.serialize.bool_type import BoolType
 from fprime.common.models.serialize.string_type import StringType
-from fprime.common.models.serialize.enum_type import EnumType
-from fprime.common.models.serialize.type_base import BaseType, ValueType
+from fprime.common.models.serialize.enum_type import EnumType, REPRESENTATION_TYPE_MAP
+from fprime.common.models.serialize.type_base import BaseType, ValueType, DictionaryType
+
+# name, desc, type
+FpyArgTemplate = tuple[str, str, type[ValueType]]
+
+
+# just for pretty-printing
+def value_type_repr(self: ValueType):
+    return str(self.val)
+
+
+ValueType.__repr__ = value_type_repr
 
 
 @dataclass
-class CmdCall:
-    node: ast.Call
-    cmd_name: str
+class FpyType(ast.AST):
+    namespace: str
+    name: str
+    fprime_type: type[ValueType]
+
+
+# this just helps the ast find what fields to pretty-print
+FpyType._fields = ["namespace", "name", "fprime_type"]
+
+
+@dataclass
+class FpyEnumConstant(ast.AST):
+    enum_type: type[EnumType]
+    repr_type: type[ValueType]
+    value: int
+
+
+FpyEnumConstant._fields = ["enum_type", "repr_type", "value"]
+
+
+@dataclass
+class FpyCmd(ast.AST):
+    namespace: str
+    name: str
     cmd_template: CmdTemplate
-    args: list
+
+
+FpyCmd._fields = ["namespace", "name", "fprime_cmd"]
 
 
 @dataclass
-class TypeInstantiate:
-    node: ast.Call
-    type_name: str
-    type_template: BaseType
-    args: list
+class FpyCh(ast.AST):
+    namespace: str
+    name: str
+    ch_template: ChTemplate
 
 
-class TransformToFPrime(ast.NodeTransformer):
+FpyCh._fields = ["namespace", "name", "fprime_ch"]
 
-    def __init__(self, cmd_name_dict: dict[str, CmdTemplate], type_name_dict: dict[str, type[ValueType]]) -> None:
+
+class ResolveNames(ast.NodeTransformer):
+
+    def __init__(
+        self,
+        cmd_name_dict: dict[str, CmdTemplate],
+        type_name_dict: dict[str, type[ValueType]],
+        ch_name_dict: dict[str, ChTemplate],
+    ) -> None:
         super().__init__()
         self.cmd_name_dict = cmd_name_dict
+        self.type_name_dict = type_name_dict
+        self.ch_name_dict = ch_name_dict
 
     def visit_Module(self, node: ast.Module):
         for statement in node.body:
@@ -66,6 +112,13 @@ class TransformToFPrime(ast.NodeTransformer):
     def visit_Attribute(self, node: ast.Attribute):
         """resolve the attribute into either a CmdTemplate instance or a BaseType type"""
 
+        # an attribute node looks like x.y
+        # attr.value = x
+        # attr.attr = y
+        # however, x in this case could be another attribute (e.g. z.w)
+        # we want to take this whole attr chain (z.w.y) and get everything except
+        # the last .y, so that we can know the namespace of this attribute
+
         namespace_node = node.value
         # concatenate namespaces
         namespace = []
@@ -78,25 +131,83 @@ class TransformToFPrime(ast.NodeTransformer):
 
         namespace_str = ".".join(namespace)
 
+        fq_name = node.attr
+        if namespace_str != "":
+            fq_name = namespace_str + "." + fq_name
+
+        # now look up this namespace string. resolve enum consts, then types, then telemetry, then commands
+        resolved_fprime_enum_type = self.type_name_dict.get(namespace_str, None)
+        if resolved_fprime_enum_type is not None:
+            if not issubclass(resolved_fprime_enum_type, EnumType):
+                node.error = "Invalid syntax"
+                return node
+
+            # this is a python integer
+            enum_value_py = resolved_fprime_enum_type.ENUM_DICT[node.attr]
+            # this is a string
+            enum_repr_type_name = resolved_fprime_enum_type.REP_TYPE
+            # this is a subclass of ValueType
+            enum_repr_type = REPRESENTATION_TYPE_MAP.get(enum_repr_type_name, None)
+            assert enum_repr_type is not None
+
+            return FpyEnumConstant(
+                resolved_fprime_enum_type, enum_repr_type, enum_value_py
+            )
+
+        resolved_fprime_type = self.type_name_dict.get(fq_name, None)
+        if resolved_fprime_type is not None:
+            return FpyType(namespace_str, node.attr, resolved_fprime_type)
+
+        resolved_fprime_ch = self.ch_name_dict.get(fq_name, None)
+        if resolved_fprime_ch is not None:
+            return FpyCh(namespace_str, node.attr, resolved_fprime_ch)
+
+        resolved_fprime_cmd = self.cmd_name_dict.get(fq_name, None)
+        if resolved_fprime_cmd is not None:
+            return FpyCmd(namespace_str, node.attr, resolved_fprime_cmd)
+
+        node.error = "Unknown identifier " + str(fq_name)
+        return node
+
+
+class ParseCalls(ast.NodeTransformer):
+
+    def __init__(self, type_name_dict: dict[str, type[ValueType]]):
+        self.type_name_dict = type_name_dict
 
     def visit_Call(self, node: ast.Call):
 
-        cmd_name = self.get_name(node.func)
-        if cmd_name not in self.cmd_name_dict:
-            node.error = "Unknown command name " + str(cmd_name)
+        # only valid calls right now are commands and dict type instantiations
+        if not isinstance(node.func, (FpyCmd, FpyType)):
+            node.error = "Invalid syntax"
             return node
 
-        cmd_template: CmdTemplate = self.cmd_name_dict[cmd_name]
-        cmd_args_map = self.map_cmd_args(cmd_template, node)
-        for fprime_arg, fpy_expr in cmd_args_map:
-            fprime_name, fprime_desc, fprime_type = fprime_arg
-            self.fpy_node_to_fprime_type(cmd_template, fprime_name, fprime_type, fpy_expr)
+        # okay, now map the args to the nodes
+        
+        if isinstance(node.func, FpyType) and not issubclass(node.func.fprime_type, DictionaryType):
+            # unknown how to construct this type
+
+        if isinstance(node.func, FpyCmd):
+
+            mapped_args = self.map_cmd_args(node.func.cmd_template, node)
+
+            for cmd_template_arg, ast_node in mapped_args.items():
+                # get the type name of the arg from the fprime type
+                fprime_type_name = [
+                    k for k, v in self.type_name_dict if v == cmd_template_arg[2]
+                ]
+                # the type should be in the dict and there should only be one of it
+                assert len(fprime_type_name) == 1
+                fprime_type_name = fprime_type_name[0]
+                fprime_value = self.cmd_template_arg_and_node_to_fprime_value(
+                    node.func.cmd_template, cmd_template_arg, fprime_type_name, ast_node
+                )
 
     def map_cmd_args(
         self, template: CmdTemplate, node: ast.Call
-    ) -> dict[tuple[str, str, ValueType], ast.Expression]:
+    ) -> dict[FpyArgTemplate, ast.AST]:
         """
-        Maps arguments from a command template to an fpy expression by position and name. Does not perform type checking.
+        Maps arguments from a command template to an ast node by position and name. Does not perform type checking.
         """
 
         mapping = dict()
@@ -129,118 +240,100 @@ class TransformToFPrime(ast.NodeTransformer):
 
         return mapping
 
-    def fpy_node_to_fprime_type(
+    def cmd_template_arg_and_node_to_fprime_value(
         self,
         cmd_template: CmdTemplate,
-        arg_name: str,
-        fprime_type_name: str,
-        fprime_type: type,
-        fpy_node: ast.Expression,
+        cmd_arg_template: FpyArgTemplate,
+        cmd_template_arg_type_name: str,
+        ast_node: ast.AST,
     ) -> ValueType:
         """
         Ensure the fpy node can be turned into the desired FPrime type
         """
 
-        def error(node, msg):
-            node.error = "In command " + str(cmd_template.get_comp_name()) + "." + str(cmd_template.get_name()) + " for argument " + str(arg_name) + ": " + str(msg)
-
-
+        arg_name = cmd_arg_template[0]
+        fprime_type = cmd_arg_template[2]
         fprime_type_instance = fprime_type()
+
+        def error(node, msg):
+            node.error = (
+                "In command "
+                + str(cmd_template.get_comp_name())
+                + "."
+                + str(cmd_template.get_name())
+                + ", argument "
+                + str(arg_name)
+                + ": "
+                + msg
+            )
+
         if issubclass(fprime_type, BoolType):
-            if not isinstance(fpy_node, ast.Constant) or not isinstance(
-                fpy_node.value, bool
-            ):
-                error(fpy_node, "Expected a boolean literal, found " + str(type(fpy_node.value)))
+            if not isinstance(ast_node, ast.Constant):
+                error(ast_node, "Invalid syntax")
                 return fprime_type_instance
-            fprime_type_instance._val = fpy_node.value
+            if not isinstance(ast_node.value, bool):
+                error(
+                    ast_node,
+                    "Expected a boolean literal, found "
+                    + str(type(ast_node.value)),
+                )
+                return fprime_type_instance
+            fprime_type_instance._val = ast_node.value
         elif issubclass(fprime_type, (F64Type, F32Type)):
-            if not isinstance(fpy_node, ast.Constant) or not isinstance(
-                fpy_node.value, float
-            ):
-                error(fpy_node, "Expected a floating point literal, found " + str(type(fpy_node.value)))
+            if not isinstance(ast_node, ast.Constant):
+                error(ast_node, "Invalid syntax")
                 return fprime_type_instance
-            fprime_type_instance._val = fpy_node.value
+            if not isinstance(ast_node.value, float):
+                error(
+                    ast_node,
+                    "Expected a floating point literal, found "
+                    + str(type(ast_node.value)),
+                )
+                return fprime_type_instance
+            fprime_type_instance._val = ast_node.value
         elif issubclass(
             fprime_type,
             (I64Type, U64Type, I32Type, U32Type, I16Type, U16Type, I8Type, U8Type),
         ):
-            if not isinstance(fpy_node, ast.Constant) or not isinstance(
-                fpy_node.value, int
-            ):
-                error(fpy_node, "Expected an integer literal, found " + str(type(fpy_node.value)))
+            if not isinstance(ast_node, ast.Constant):
+                error(ast_node, "Invalid syntax")
                 return fprime_type_instance
-            fprime_type_instance._val = fpy_node.value
+            if not isinstance(ast_node.value, float):
+                error(
+                    ast_node,
+                    "Expected an integer literal, found "
+                    + str(type(ast_node.value)),
+                )
+                return fprime_type_instance
+            fprime_type_instance._val = ast_node.value
         elif issubclass(fprime_type, StringType):
-            if not isinstance(fpy_node, ast.Constant) or not isinstance(
-                fpy_node.value, str
-            ):
-                error(fpy_node, "Expected a string literal, found " + str(type(fpy_node.value)))
+            if not isinstance(ast_node, ast.Constant):
+                error(ast_node, "Invalid syntax")
                 return fprime_type_instance
-            fprime_type_instance._val = fpy_node.value
+            if not isinstance(ast_node.value, float):
+                error(
+                    ast_node,
+                    "Expected an integer literal, found "
+                    + str(type(ast_node.value)),
+                )
+                return fprime_type_instance
+            fprime_type_instance._val = ast_node.value
         elif issubclass(fprime_type, EnumType):
-
-            # all enums follow the form Fprime.Qualified.Type.ENUM_CONST_NAME
-            if not isinstance(fpy_node, ast.Attribute):
-                # not an enum literal
-                error(fpy_node, "Expected an enum literal")
+            if not isinstance(ast_node, FpyEnumConstant):
+                error(ast_node, "Invalid syntax")
                 return fprime_type_instance
-
-            fpy_enum_name = self.get_name(fpy_node.value) # fpy_node.value is the fqn of the enum
-            if fpy_enum_name is None:
-                # unable to get the name from this ast
-                error(fpy_node, "Invalid syntax")
-                return fprime_type_instance
-
-            resolved_enum_type = self.resolve_type_name(fpy_enum_name)
-            if resolved_enum_type is None:
-                # couldn't find this enum in the type dict
-                error(fpy_node, "Unknown type " + str(fpy_enum_name))
-                return fprime_type_instance
-
-            if resolved_enum_type != fprime_type:
-                # wrong type
-                error(fpy_node, "Expected a value from enum " + str(fprime_type.__name__) + " but got one from " + str(resolved_enum_type.__name__))
-                return fprime_type_instance
-
-            fprime_type_instance._val = fpy_node.attr # fpy_node.attr is ENUM_CONST_NAME
-
+            assert fprime_type == ast_node.enum_type
+            fprime_type_instance._val = ast_node.value
         elif issubclass(fprime_type, (ArrayType, SerializableType)):
 
+            if not isinstance(ast_node, FpyType):
 
-            
-            # all arrays and structs follow the form Fprime.Qualified.Type(<args>)
-            fpy_type_name = self.get_name(fpy_node.value) # fpy_node.value is the fqn of the enum
-            if fpy_enum_name is None:
-                # unable to get the name from this ast
-                fpy_node.error = "Invalid syntax"
-                return fprime_type_instance
-
-            resolved_enum_type = self.resolve_type_name(fpy_enum_name)
-            if resolved_enum_type is None:
-                # couldn't find this enum in the type dict
-                fpy_node.error = "Unknown type " + str(fpy_enum_name)
-                return fprime_type_instance
-
-            if resolved_enum_type != fprime_type:
-                # wrong enum
-                fpy_node.error = "Expected a value from enum " + str(fprime_type.__name__) + " but got one from " + str(resolved_enum_type.__name__)
-                return fprime_type_instance
-
-            fprime_type_instance._val = fpy_node.attr
 
     def node_to_fprime_type(self, node: ast.Expression, into_type: ValueType) -> bool:
         """
         Turn the node into an instance of the desired FPrime type
         """
         return False
-
-    def get_name(self, name: ast.Expression) -> str|None:
-
-    def resolve_cmd_name(self, cmd_name: str) -> CmdTemplate|None:
-        return self.cmd_name_dict.get(cmd_name, None)
-
-    def resolve_type_name(self, enum_name: str) -> type[ValueType]|None:
-        return self.type_name_dict.get(enum_name, str)
 
     def process_args(self, input_values):
         """Process input arguments"""
@@ -305,11 +398,20 @@ def main():
 
 def compile(node: ast.Module, dictionary: Path):
     print(ast.dump(node, indent=4))
-    cmd_json_dict = CmdJsonLoader(dictionary)
-    (cmd_id_dict, cmd_name_dict, versions) = cmd_json_dict.construct_dicts(dictionary)
-    type_name_dict = cmd_json_dict.parsed_types
-    py_to_fprime_transformer = TransformToFPrime(cmd_name_dict, type_name_dict)
-    node = py_to_fprime_transformer.visit(node)
+
+    cmd_json_dict_loader = CmdJsonLoader(dictionary)
+    (cmd_id_dict, cmd_name_dict, versions) = cmd_json_dict_loader.construct_dicts(
+        dictionary
+    )
+    type_name_dict = cmd_json_dict_loader.parsed_types
+    ch_json_dict_loader = ChJsonLoader(dictionary)
+    (ch_id_dict, ch_name_dict, versions) = ch_json_dict_loader.construct_dicts(
+        dictionary
+    )
+    name_resolver = ResolveNames(cmd_name_dict, type_name_dict, ch_name_dict)
+    node = name_resolver.visit(node)
+
+    print(ast.dump(node, indent=4))
     if not check_for_errors(node):
         return 1
 
--- test/fprime_gds/common/sequence/test_basic_seq.py
+++ test/fprime_gds/common/sequence/test_basic_seq.py
@@ -4,6 +4,20 @@ from fprime_gds.common.sequence.compiler import compile
 def test_basic_seq():
     seq = \
 """
-Ref.fileManager.RemoveFile(Ref.MyEnum.TEST, arg=Ref.MyType(asdf))
+Ref.fileManager.RemoveFile("test", False)
+"""
+    assert compile(ast.parse(seq), Path(__file__).parent / "RefTopologyDictionary.json") == 0
+
+def test_enum_arg():
+    seq = \
+"""
+Ref.typeDemo.CHOICE(Ref.Choice.ONE)
+"""
+    assert compile(ast.parse(seq), Path(__file__).parent / "RefTopologyDictionary.json") == 0
+
+def test_struct_arg():
+    seq = \
+"""
+Ref.typeDemo.CHOICE_PAIR(Ref.ChoicePair(Ref.Choice.ONE, Ref.Choice.TWO))
 """
     assert compile(ast.parse(seq), Path(__file__).parent / "RefTopologyDictionary.json") == 0
