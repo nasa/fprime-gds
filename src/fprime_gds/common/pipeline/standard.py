5c29924c154a034ca5c9ccb70c55ac81f5fe6b22
jawest <justinewest14@gmail.com>
2025-05-09 12:05:55 -0700

    Check to see if fw type dict is populated

--- src/fprime_gds/common/pipeline/standard.py
+++ src/fprime_gds/common/pipeline/standard.py
@@ -87,8 +87,9 @@ class StandardPipeline:
         # Setup dictionaries encoders and decoders
         self.dictionaries.load_dictionaries(self.dictionary_path, packet_spec, packet_set_name)
         # Update config to use Fw types defined in the JSON dictionary
-        for fw_type_name, fw_type in self.dictionaries._fw_type_name_dict.items():
-            config.set("types", fw_type_name, fw_type)
+        if self.dictionaries._fw_type_name_dict:
+            for fw_type_name, fw_type in self.dictionaries._fw_type_name_dict.items():
+                config.set("types", fw_type_name, fw_type)
         self.coders.setup_coders(
             self.dictionaries, self.distributor, self.client_socket, config
         )
