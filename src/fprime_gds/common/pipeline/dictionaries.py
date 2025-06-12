a3c348efd17602b3659d901f5b696b50c23b124d
M Starch <LeStarch@googlemail.com>
2025-06-12 15:59:34 -0700

    Adding in default packet set selection

--- src/fprime_gds/common/pipeline/dictionaries.py
+++ src/fprime_gds/common/pipeline/dictionaries.py
@@ -128,8 +128,15 @@ class Dictionaries:
             msg = f"[ERROR] Dictionary '{dictionary}' does not exist."
             raise Exception(msg)
         # Check for packet specification
-        if self._metadata["dictionary_type"] == "json" and packet_set_name is not None:
+        if self._metadata["dictionary_type"] == "json":
             packet_loader = fprime_gds.common.loaders.pkt_json_loader.PktJsonLoader(dictionary)
+            if packet_set_name is None:
+                names = packet_loader.get_packet_set_names(None)
+                if len(names) == 0:
+                    self._packet_dict = None
+                elif len(names) > 1:
+                    raise Exception("[ERROR] Multiple packet sets, must set --packet-set-name")
+                packet_set_name = names[0]
             self._packet_dict = packet_loader.get_id_dict(
                 None, packet_set_name, self._channel_name_dict
             )
