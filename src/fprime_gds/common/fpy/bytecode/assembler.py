048c20e194b1e398ba5ccd8fbaa331935437b835
Zimri Leisher <zimri.leisher@fireflyspace.com>
2025-07-08 11:16:29 -0500

    Add some bytecode assembler work

---
+++ src/fprime_gds/common/fpy/bytecode/assembler.py
@@ -0,0 +1,87 @@
+from dataclasses import astuple, dataclass
+import struct
+import zlib
+from fprime_gds.common.fpy.bytecode.directives import Directive
+from fprime_gds.common.fpy.bytecode.parser import (
+    AstBody,
+    AstDirStmt,
+    AstGotoTagStmt,
+    parse,
+)
+import argparse
+from pathlib import Path
+
+HEADER_FORMAT = "!BBBBBHI"
+HEADER_SIZE = struct.calcsize(HEADER_FORMAT)
+
+
+@dataclass
+class Header:
+    majorVersion: int
+    minorVersion: int
+    patchVersion: int
+    schemaVersion: int
+    argumentCount: int
+    statementCount: int
+    bodySize: int
+
+
+FOOTER_FORMAT = "!I"
+FOOTER_SIZE = struct.calcsize(FOOTER_FORMAT)
+
+
+@dataclass
+class Footer:
+    crc: int
+
+
+def serialize_directives(dirs: list[Directive], output: Path):
+    output_bytes = bytes()
+
+    for dir in dirs:
+        output_bytes += dir.serialize()
+
+    header = Header(0, 0, 0, 1, 0, len(dirs), len(output_bytes))
+    output_bytes = struct.pack(HEADER_FORMAT, *astuple(header)) + output_bytes
+
+    crc = zlib.crc32(output_bytes) % (1 << 32)
+    footer = Footer(crc)
+    output_bytes += struct.pack(FOOTER_FORMAT, *astuple(footer))
+    output.write_bytes(output_bytes)
+
+
+def assemble(body: AstBody):
+    directive_idx = 0
+    gotos: dict[str, int] = {}
+    for stmt in body:
+        if isinstance(stmt, AstGotoTagStmt):
+            gotos[stmt.tag] = directive_idx + 1
+        else:
+            assert isinstance(stmt, AstDirStmt), stmt
+            directive_idx += 1
+
+
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("input", type=Path, help="The input .fpybc file")
+    arg_parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        required=False,
+        default=None,
+        help="The output .bin path",
+    )
+
+    args = arg_parser.parse_args()
+
+    if not args.input.exists():
+        print(f"Input file {args.input} does not exist")
+        exit(-1)
+
+    body = parse(args.input.read_text())
+    directives = assemble(body, args.dictionary)
+    output = args.output
+    if output is None:
+        output = args.input.with_suffix(".bin")
+    serialize_directives(directives, output)
---
+++ src/fprime_gds/common/fpy/bytecode/parser.py
@@ -0,0 +1,236 @@
+from dataclasses import dataclass, field, fields
+from pathlib import Path
+from pprint import pprint
+from lark.indenter import PythonIndenter
+from lark import Lark, Transformer, ast_utils, v_args
+from lark.tree import Meta
+
+fpy_grammar_str = (Path(__file__).parent / "grammar.lark").read_text()
+
+input_text = None
+
+
+def parse(text: str):
+    parser = Lark(
+        fpy_grammar_str,
+        start="input",
+        parser="lalr",
+        postlex=PythonIndenter(),
+        propagate_positions=True,
+        maybe_placeholders=True,
+    )
+
+    global input_text
+    input_text = text
+    tree = parser.parse(text, on_error=lambda x: print("Error"))
+    transformed = FpyBytecodeTransformer().transform(tree)
+    return transformed
+
+
+@dataclass
+class Ast:
+    meta: Meta = field(repr=False)
+    id: int = field(init=False, repr=False, default=None)
+    node_text: str = field(init=False, repr=False, default=None)
+
+    def __post_init__(self):
+        if not hasattr(self.meta, "start_pos"):
+            self.node_text = ""
+            return
+        self.node_text = (
+            input_text[self.meta.start_pos : self.meta.end_pos]
+            .replace("\n", " ")
+            .strip()
+        )
+
+    def __hash__(self):
+        return hash(self.id)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.node_text})"
+
+
+@dataclass
+class AstGotoTagStmt(Ast):
+    tag: str
+
+
+@dataclass
+class AstBinaryRegOpCall:
+    op: str
+    lhs: int
+    rhs: int
+    res: int
+
+
+@dataclass
+class AstUnaryRegOpCall:
+    op: str
+    src: int
+    res: int
+
+
+@dataclass
+class AstGotoCall(Ast):
+    dest: str | int
+
+
+@dataclass
+class AstWaitRelCall(Ast):
+    seconds: int
+    useconds: int
+
+
+@dataclass
+class AstWaitAbsCall(Ast):
+    time_context: int
+    time_base: int
+    seconds: int
+    useconds: int
+
+
+@dataclass
+class AstSetSerRegCall(Ast):
+    ser_reg: int
+    value: list[int] | None
+
+
+@dataclass
+class AstIfCall(Ast):
+    conditional_reg: int
+    false_goto: int | str
+
+
+@dataclass
+class AstNoOpCall(Ast):
+    pass
+
+
+@dataclass
+class AstGetTlmCall(Ast):
+    value_reg: int
+    time_reg: int
+    chan_id: int
+
+
+@dataclass
+class AstGetPrmCall(Ast):
+    value_reg: int
+    prm_id: int
+
+
+@dataclass
+class AstCmdCall(Ast):
+    opcode: int
+    value: list[int] | None
+
+
+@dataclass
+class AstDeserSerRegCall(Ast):
+    src_ser_reg: int
+    src_offset: int
+    dest_reg: int
+    deser_size: int
+
+
+@dataclass
+class AstSetRegCall(Ast):
+    reg: int
+    value: int
+
+
+@dataclass
+class AstExitCall(Ast):
+    success: bool
+
+
+AstDirStmt = (
+    AstWaitRelCall
+    | AstWaitAbsCall
+    | AstGotoCall
+    | AstBinaryRegOpCall
+    | AstUnaryRegOpCall
+    | AstSetSerRegCall
+    | AstSetRegCall
+    | AstIfCall
+    | AstNoOpCall
+    | AstGetPrmCall
+    | AstGetTlmCall
+    | AstCmdCall
+    | AstDeserSerRegCall
+    | AstExitCall
+)
+
+AstStmt = AstGotoTagStmt | AstDirStmt
+
+
+@dataclass
+class AstBody(Ast):
+    stmts: list[AstStmt]
+
+
+for cls in Ast.__subclasses__():
+    cls.__hash__ = Ast.__hash__
+    # cls.__repr__ = Ast.__repr__
+
+
+@v_args(meta=False, inline=False)
+def as_list(self, tree):
+    return list(tree)
+
+
+def no_inline_or_meta(type):
+    @v_args(meta=False, inline=False)
+    def wrapper(self, tree):
+        return type(tree)
+
+    return wrapper
+
+
+def no_inline(type):
+    @v_args(meta=True, inline=False)
+    def wrapper(self, meta, tree):
+        return type(meta, tree)
+
+    return wrapper
+
+
+def no_meta(type):
+    @v_args(meta=False, inline=True)
+    def wrapper(self, tree):
+        return type(tree)
+
+    return wrapper
+
+
+def handle_str(meta, s: str):
+    return s.strip("'").strip('"')
+
+
+@v_args(meta=True, inline=True)
+class FpyBytecodeTransformer(Transformer):
+    input = no_inline(AstBody)
+    goto_tag_stmt = AstGotoTagStmt
+    binary_reg_op_call = AstBinaryRegOpCall
+    unary_reg_op_call = AstUnaryRegOpCall
+    goto_call = AstGotoCall
+    wait_rel_call = AstWaitRelCall
+    wait_abs_call = AstWaitAbsCall
+    set_ser_reg_call = AstSetSerRegCall
+    if_call = AstIfCall
+    no_op_call = AstNoOpCall
+    get_tlm_call = AstGetTlmCall
+    get_prm_call = AstGetPrmCall
+    cmd_call = AstCmdCall
+    deser_ser_reg_call = AstDeserSerRegCall
+    set_reg_call = AstSetRegCall
+    exit_call = AstExitCall
+    binary_reg_op = no_meta(str)
+    name = no_meta(str)
+    NAME = str
+    DEC_NUMBER = int
+    FLOAT_NUMBER = float
+    COMPARISON_OP = str
+    STRING = handle_str
+    CONST_TRUE = lambda a, b: True
+    CONST_FALSE = lambda a, b: False
