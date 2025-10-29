abac6997847fca3dffa301ae5672c75a851cb993
thomas-bc <thomas.boyerchammard@gmail.com>
2025-10-29 10:40:47 -0700

    Fix empty fprime-gds.yml options error

--- src/fprime_gds/executables/cli.py
+++ src/fprime_gds/executables/cli.py
@@ -374,6 +374,8 @@ class ConfigDrivenParser(ParserBase):
     def flatten_options(configured_options):
         """Flatten options down to arguments"""
         flattened = []
+        if not configured_options:
+            return flattened
         for option, value in configured_options.items():
             flattened.append(f"--{option}")
             if value is not None:
