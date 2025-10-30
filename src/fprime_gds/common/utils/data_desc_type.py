b20a606b52d5fa3896ba65a2724f36474d3259ed
thomas-bc <thomas.boyerchammard@gmail.com>
2025-10-29 18:44:43 -0700

    Python is funnnn

--- src/fprime_gds/common/utils/data_desc_type.py
+++ src/fprime_gds/common/utils/data_desc_type.py
@@ -10,61 +10,66 @@ from fprime_gds.common.utils.config_manager import ConfigManager
 
 
 class MetaDescType(type):
+    """Metaclass for DataDescType to allow dynamically loading enum values"""
+
+    LOADED: bool = False
+    UNDERLYING_ENUM: type[Enum] = Enum(
+        "DataDescType",
+        # Default values - will be overridden if loaded from ConfigManager
+        {
+            # Command packet type - incoming
+            "FW_PACKET_COMMAND": 0,
+            # Telemetry packet type - outgoing
+            "FW_PACKET_TELEM": 1,
+            # Log type - outgoing
+            "FW_PACKET_LOG": 2,
+            # File type - incoming and outgoing
+            "FW_PACKET_FILE": 3,
+            # Packetized telemetry packet type
+            "FW_PACKET_PACKETIZED_TLM": 4,
+            # Idle packet
+            "FW_PACKET_IDLE": 5,
+            # Handshake packet
+            "FW_PACKET_HAND": 0xFE,
+            # Unknown packet
+            "FW_PACKET_UNKNOWN": 0xFF,
+            # Space Packet Idle APID
+            "CCSDS_SPACE_PACKET_IDLE_APID": 0x7FF,
+        },
+    )
+
     def __call__(self, *args: Any, **kwds: Any) -> Any:
-        TmpDataDescType = Enum(
-            "DataDescType",
-            ConfigManager.get_instance().get_type("ComCfg.Apid").ENUM_DICT,
-        )
-        return TmpDataDescType(*args, **kwds)
+        self.load_guard()
+        return self.UNDERLYING_ENUM(*args, **kwds)
 
     def __getitem__(self, item: Any) -> Any:
-        TmpDataDescType = Enum(
-            "DataDescType",
-            ConfigManager.get_instance().get_type("ComCfg.Apid").ENUM_DICT,
-        )
-        return TmpDataDescType[item]
+        self.load_guard()
+        return self.UNDERLYING_ENUM[item]
 
     def __iter__(self):
-        TmpDataDescType = Enum(
-            "DataDescType",
-            ConfigManager.get_instance().get_type("ComCfg.Apid").ENUM_DICT,
-        )
-        return iter(TmpDataDescType)
+        self.load_guard()
+        return iter(self.UNDERLYING_ENUM)
+
+    @classmethod
+    def load_guard(cls):
+        if not cls.LOADED:
+            cls.LOADED = True
+            # Load the enum values from the config manager
+            apid_type = ConfigManager.get_instance().get_type("ComCfg.Apid")
+            if apid_type is not None:
+                cls.UNDERLYING_ENUM = Enum(
+                    "DataDescType",
+                    apid_type.ENUM_DICT,
+                )
 
 
 class DataDescType(metaclass=MetaDescType):
-    """DataDescType is a class whose purpose is to behave like an Enum, but the values
-    are dynamically loaded at call-time. This allows for values to be configured through
-    ConfigManager, which loads the ComCfg.Apid object from the dictionary.
+    """DEPRECATED: Use ConfigManager.get_data_desc_type() instead
 
-    Args:
-        metaclass (_type_, optional): _description_. Defaults to MetaDescType.
+    DataDescType is a class whose purpose is to behave like an Enum, but the values
+    are dynamically loaded from the ConfigManager the first time it is accessed.
+    This allows for values to be configured through the items from the dictionary.
     """
 
     value: int
     name: str
-
-
-# DataDescType = Enum(
-#     "DataDescType",
-#     {
-#         # Command packet type - incoming
-#         "FW_PACKET_COMMAND": 0,
-#         # Telemetry packet type - outgoing
-#         "FW_PACKET_TELEM": 1,
-#         # Log type - outgoing
-#         "FW_PACKET_LOG": 2,
-#         # File type - incoming and outgoing
-#         "FW_PACKET_FILE": 3,
-#         # Packetized telemetry packet type
-#         "FW_PACKET_PACKETIZED_TLM": 4,
-#         # Idle packet
-#         "FW_PACKET_IDLE": 5,
-#         # Handshake packet
-#         "FW_PACKET_HAND": 0xFE,
-#         # Unknown packet
-#         "FW_PACKET_UNKNOWN": 0xFF,
-#         # Space Packet Idle APID
-#         "CCSDS_SPACE_PACKET_IDLE_APID": 0x7FF,
-#     },
-# )
