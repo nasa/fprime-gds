4e2481cce0bfb1ee004b65ebf9940b9e669e0b37
zimri-leisher <zimri.leisher@gmail.com>
2025-11-09 20:48:03 -0500

    sp

--- src/fprime_gds/common/fpy/semantics.py
+++ src/fprime_gds/common/fpy/semantics.py
@@ -421,7 +421,7 @@ class ResolveVarsTypesAndFuncs(TopDownVisitor):
         # don't need to do anything for literals or getattr, but just have this here for completion's sake
         # the reason we don't need to do anything for getattr is because the point of this
         # pass is explicitly not to resolve getattrs, just resolve vars (and types), not general
-        # getattrs
+        # getattr
         # we don't do this now because we need to resolve all expr types first
         # before we can figure out whether a getattr is correct, and this pass lets
         # us now do that (i.e. after this pass, we have enough info about the program
