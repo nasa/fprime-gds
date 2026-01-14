92c897499fea2de86b4bee8bef47f1f8d028c8ac
Nate Gay <email@nategay.me>
2026-01-13 20:40:41 -0600

    Kinda nice

--- src/fprime_gds/flask/static/addons/confetti/addon.js
+++ src/fprime_gds/flask/static/addons/confetti/addon.js
@@ -34,139 +34,127 @@ Vue.component("confetti-trigger", {
     },
     methods: {
         triggerConfetti() {
-            tsParticles.load({
-                id: "tsparticles",
-                options: {
-                    "fullScreen": {
-                        "zIndex": 1
+
+            const confettiEmitter = (direction, position) => {
+                return {
+                    life: {
+                        count: 1,
+                        duration: 2,
                     },
-                    "particles": {
-                        "number": {
-                        "value": 0
+                    direction,
+                    rate: {
+                        quantity: 25,
+                        delay: 0.2
+                    },
+                    position
+                };
+            };
+
+            const options = {
+                particles: {
+                    angle: {
+                        value: 0,
+                        offset: 30
+                    },
+                    move: {
+                        enable: true,
+                        outModes: {
+                            top: "none",
+                            default: "destroy"
                         },
-                        "color": {
-                        "value": [
-                            "#00FFFC",
-                            "#FC00FF",
-                            "#fffc00"
-                        ]
+                        gravity: {
+                            enable: true
                         },
-                        "shape": {
-                        "type": [
+                        speed: { min: 2, max: 20 },
+                        decay: 0.01
+                    },
+                    number: {
+                        value: 0,
+                        limit: 300
+                    },
+                    opacity: {
+                        value: 1
+                    },
+                    color: {
+                    value: [
+                        "#00FFFC",
+                        "#FC00FF",
+                        "#fffc00",
+                    ]
+                    },
+                    shape: {
+                        type: [
                             "circle",
                             "square",
-                            "triangle"
+                            "triangle",
                         ],
-                        "options": {}
-                        },
-                        "opacity": {
-                        "value": {
-                            "min": 0,
-                            "max": 1
-                        },
-                        "animation": {
-                            "enable": true,
-                            "speed": 2,
-                            "startValue": "max",
-                            "destroy": "min"
-                        }
-                        },
-                        "size": {
-                        "value": {
-                            "min": 2,
-                            "max": 4
-                        }
-                        },
-                        "links": {
-                        "enable": false
-                        },
-                        "life": {
-                        "duration": {
-                            "sync": true,
-                            "value": 5
-                        },
-                        "count": 1
-                        },
-                        "move": {
-                        "enable": true,
-                        "gravity": {
-                            "enable": true,
-                            "acceleration": 10
-                        },
-                        "speed": {
-                            "min": 10,
-                            "max": 20
-                        },
-                        "decay": 0.1,
-                        "direction": "none",
-                        "straight": false,
-                        "outModes": {
-                            "default": "destroy",
-                            "top": "none"
-                        }
-                        },
-                        "rotate": {
-                        "value": {
-                            "min": 0,
-                            "max": 360
-                        },
-                        "direction": "random",
-                        "move": true,
-                        "animation": {
-                            "enable": true,
-                            "speed": 60
-                        }
-                        },
-                        "tilt": {
-                        "direction": "random",
-                        "enable": true,
-                        "move": true,
-                        "value": {
-                            "min": 0,
-                            "max": 360
-                        },
-                        "animation": {
-                            "enable": true,
-                            "speed": 60
-                        }
-                        },
-                        "roll": {
-                        "darken": {
-                            "enable": true,
-                            "value": 25
-                        },
-                        "enable": true,
-                        "speed": {
-                            "min": 15,
-                            "max": 25
-                        }
-                        },
-                        "wobble": {
-                        "distance": 30,
-                        "enable": true,
-                        "move": true,
-                        "speed": {
-                            "min": -15,
-                            "max": 15
-                        }
+                    },
+                    size: {
+                        value: { min: 2, max: 6 },
+                        animation: {
+                            count: 1,
+                            startValue: "max",
+                            destroy: "min",
+                            enable: true,
+                            speed: 0.01,
+                            sync: true
                         }
                     },
-                    "emitters": {
-                        "life": {
-                        "count": 1,
-                        "duration": 0.1,
-                        "delay": 0.4
+                    rotate: {
+                        value: {
+                            min: 0,
+                            max: 360
                         },
-                        "rate": {
-                        "delay": 0.1,
-                        "quantity": 150
+                        direction: "random",
+                        animation: {
+                            enable: true,
+                            speed: 60
+                        }
+                    },
+                    tilt: {
+                        direction: "random",
+                        enable: true,
+                        value: {
+                            min: 0,
+                            max: 360
                         },
-                        "size": {
-                        "width": 0,
-                        "height": 0
+                        animation: {
+                            enable: true,
+                            speed: 60
+                        }
+                    },
+                    roll: {
+                        darken: {
+                            enable: true,
+                            value: 25
+                        },
+                        enable: true,
+                        speed: {
+                            min: 15,
+                            max: 25
+                        }
+                    },
+                    wobble: {
+                        distance: 30,
+                        enable: true,
+                        speed: {
+                            min: -15,
+                            max: 15
                         }
                     }
-                }
+                },
+                emitters: [
+                    confettiEmitter("top-right", { x: 0, y: 0 }),
+                    confettiEmitter("top-center", { x: 25, y: 0 }),
+                    confettiEmitter("top-center", { x: 50, y: 0 }),
+                    confettiEmitter("top-center", { x: 75, y: 0 }),
+                    confettiEmitter("top-left", { x: 100, y: 0 })
+                ]
+            };
+
+            tsParticles.load({
+                id: "tsparticles",
+                options: options,
             });
         }
     }
