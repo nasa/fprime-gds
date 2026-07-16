d7790d1eb7dd127717a0f9983b08f8fb2ac7467c
Yukti Vijay <yvijay@umich.edu>
2026-07-16 10:32:04 -0700

    changed transport to suit boolean type in fprime-xtce

--- src/fprime_gds/common/yamcs_transport.py
+++ src/fprime_gds/common/yamcs_transport.py
@@ -177,7 +177,8 @@ class YamcsClient(TransportClient):
         for i, spec in enumerate(template.get_args()):
             val = arg_vals[i].val
             if isinstance(val, bool):
-                val = "True" if val else "False"
+                # Convert Python boolean to F Prime wire format: True=255, False=0
+                val = "255" if val else "0"
             else:
                 val = str(val)
             args_dict[spec[0]] = val
